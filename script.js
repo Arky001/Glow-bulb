f87767b0923deb355505b0fb1cab5e28ca8c1f01
ABHAY RAJ <85006031+Arky001@users.noreply.github.com>
2023-05-31 02:52:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function bulb_off() {
+    document.getElementById('bulb').src = './image/off.jpg';
+}
+
+function bulb_on() {
+    document.getElementById('bulb').src = './image/yellow.jpg';
+}
+
+function bulb_red() {
+    document.getElementById('bulb').src = './image/red.jpg';
+}
+
+function bulb_blue() {
+    document.getElementById('bulb').src = './image/blue.jpg';
+}
+
+function bulb_green() {
+    document.getElementById('bulb').src = './image/green.jpg';
+}
+
+function bulb_pink() {
+    document.getElementById('bulb').src = './image/pink.jpg';
+}
